0da99c0eb848754d01f1b3b9f172ac7d3910eb06
sbcesar <148891359+sbcesar@users.noreply.github.com>
2025-02-11 12:50:52 +0100

    ahora funciona para cualquier ubicacion mientras el txt de las contraseñas se encuentre en el proyecto

--- FuerzaBruta/Program.cs
+++ FuerzaBruta/Program.cs
@@ -4,9 +4,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string filePath = @"C:\\Users\\cesar\\Desktop\\2DAM\\PSPCSharp\\FuerzaBruta\\FuerzaBruta\\2151220-passwords.txt";
+        string fileName = "2151220-passwords.txt";
+
+        // Subir tres niveles desde la carpeta bin para llegar a la raíz del proyecto
+        string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+
+        // Construir la ruta final al archivo
+        string filePath = Path.Combine(projectDirectory, fileName);
         
         HashDirectory.HashArguments(filePath);
+        
     }
 }
-
